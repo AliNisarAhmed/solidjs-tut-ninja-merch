6181cf74c97a88299691ef527885060edbe3dcc3
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2023-06-01 20:05:43 -0600

    4: setup and basic styling with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import type { Component } from "solid-js";
+import Card from "./components/Card";
+import banner from "./assets/banner.png";
+
+const App: Component = () => {
+  return (
+    <div class="container m-auto">
+      <header>
+        <h1>Ninja Merch</h1>
+      </header>
+      <img class="rounded-md" src={banner} alt="site banner" />
+      <div class="grid grid-cols-4 gap-10 m-4">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div class="bg-white p-4 text-center rounded-md shadow-ms">
+      <h2>Card Component</h2>
+      <button class="btn">Click me</button>
+    </div>
+  );
+}
